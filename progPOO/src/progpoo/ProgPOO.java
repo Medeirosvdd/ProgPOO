c363a49f1863b1291bc4b92af9fea3009515cbcf
unknown <Administrador@CSL-P5A3385249.ensino.ulbra.net>
2025-09-01 22:12:46 -0300

    a

---
+++ progPOO/src/progpoo/ProgPOO.java
@@ -0,0 +1,13 @@
+package progpoo;
+
+import view.FrLogin;
+
+
+public class ProgPOO {
+
+    public static void main(String[] args) {
+               FrLogin telaLogin = new  FrLogin();
+               telaLogin.setVisible(true);
+    }
+    
+}
